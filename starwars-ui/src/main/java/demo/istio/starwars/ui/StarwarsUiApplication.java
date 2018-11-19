9700db52ff7a476026f156de00b9f50515f2dd76
Serge Bishyr <seriybg@gmail.com>
2018-11-19 17:12:50 +0200

    add empty ui service

---
+++ starwars-ui/src/main/java/demo/istio/starwars/ui/StarwarsUiApplication.java
@@ -0,0 +1,12 @@
+package demo.istio.starwars.ui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarwarsUiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StarwarsUiApplication.class, args);
+    }
+}
