e893b0c7d52ac2279e621ffd4c3ed66b7dc6a5de
Serge Bishyr <seriybg@gmail.com>
2018-11-25 22:40:13 +0200

    add deathstar call from ui

--- starwars-ui-new/index.js
+++ starwars-ui-new/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const request = require('request')
 const app = express()
+const PORT = 3000
 
 app.use(express.static('public'));
 app.set('view engine', 'ejs')
@@ -14,6 +15,11 @@ app.get('/quote', function(req, res) {
     request(quoteServiceUrl + "/quote", function(err, response, body) {
         if (err) {
             res.render("quote", {quote: null, error: err})
+            return;
+        }
+        if (!response) {
+            res.render("quote", {quote: null, error: "Unable to parse response"})
+            return;
         }
         res.render("quote", {quote: JSON.parse(response.body), error: null})
     })
@@ -21,9 +27,20 @@ app.get('/quote', function(req, res) {
 
 app.get('/dethstar', function(req, res) {
     deathStarServiceUrl = process.env.DEATH_STAR_SERVICE_URL || "http://localhost:8082"
+    request(deathStarServiceUrl + "/destroy", function(err, response, body) {
+        if (err) {
+            res.render("dethstar", {planet: null, error: err})
+            return;
+        }
+        if (!response) {
+            res.render("dethstar", {planet: null, error: "Unable to parse response"})
+            return;
+        }
+        res.render("dethstar", {planet: JSON.parse(response.body), error: null})
+    })
 })
 
-app.listen(3000, function () {
+app.listen(PORT, function () {
   console.log('Serving on port 3000...')
 })
 
